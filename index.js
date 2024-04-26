8ab828c899b1bc62edbf53b5016a174bae8d5f24
Nizam uddin Sangram <nizamuddinstruggle@gmail.com>
2024-04-26 19:43:56 +0600

    try to find single data for backend

--- index.js
+++ index.js
@@ -52,6 +52,9 @@ async function run() {
     app.get("/myPlaces/:id", async (req, res) => {
       const id = req.params.id;
       console.log(id);
+      const query = { _id: new ObjectId(id) };
+      const result = await placesCollection.findOne(query);
+      res.send(result);
     });
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
